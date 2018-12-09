8c7103d3fde8a6d587204a8091b9c4bc6c8f0861
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-12-09 11:53:50 -0500

    finish index

--- src/reader.js
+++ src/reader.js
@@ -104,11 +104,77 @@ function compiler(tempFile){
             "state": state,
             "tag": tag,
           };
+        } else {
+          if(!globalScope.functions[scope]) throw(`Current scope does not exist. ${name}`);
+          globalScope.functions[scope].runners.push({
+            "type": "func",
+            "name": name,
+            "state": state,
+            "tag": tag,
+          });
         }
         break;
       case "construct":
+        if(!check[1]) throw("Missing variable name;");
+        if(check[2] !== ":") throw("Missing symbol: ':';");
+        if(check[3] !== "(") throw("Missing symbol: '(';");
+        if(!check[4]) throw("Missing construct type;");
+        if(check[5] !== ")") throw("Missing symbol: ')';");
+        if(check[6] !== "[") throw("Missing symbol: '[';");
+        if(check[7] !== "global" || check[7] !== "local" || check[8] !== "private" || check[9] !== "constant") throw("Invalid tag provided;");
+        if(check[8] !== "]") throw("Missing symbol: ']';");
+        if(check[9] !== "|'") throw("Missing symbol: '|';");
+        let name = check[1];
+        let type = check[4];
+        let tag = check[7];
+        if(scope == "global"){
+          if(globalScope.functions[name]) throw(`Function: '${name}', is already defined.`);
+          globalScope.functions[name] = {
+            "type": "construct",
+            "name": name,
+            "state": type,
+            "tag": tag,
+          };
+        } else {
+          if(!globalScope.functions[scope]) throw(`Current scope does not exist. ${name}`);
+          globalScope.functions[scope].runners.push({
+            "type": "construct",
+            "name": name,
+            "state": type,
+            "tag": tag,
+          });
+        }
         break;
       case "index":
+        if(!check[1]) throw("Missing variable name;");
+        if(check[2] !== ":") throw("Missing symbol: ':';");
+        if(check[3] !== "(") throw("Missing symbol: '(';");
+        if(!check[4]) throw("Missing index location;");
+        if(check[5] !== ")") throw("Missing symbol: ')';");
+        if(check[6] !== "[") throw("Missing symbol: '[';");
+        if(check[7] !== "global" || check[7] !== "local" || check[8] !== "private" || check[9] !== "constant") throw("Invalid tag provided;");
+        if(check[8] !== "]") throw("Missing symbol: ']';");
+        if(check[9] !== "|'") throw("Missing symbol: '|';");
+        let name = check[1];
+        let type = check[4];
+        let tag = check[7];
+        if(scope == "global"){
+          if(globalScope.functions[name]) throw(`Function: '${name}', is already defined.`);
+          globalScope.functions[name] = {
+            "type": "construct",
+            "name": name,
+            "state": type,
+            "tag": tag,
+          };
+        }  else {
+          if(!globalScope.functions[scope]) throw(`Current scope does not exist. ${name}`);
+          globalScope.functions[scope].runners.push({
+            "type": "construct",
+            "name": name,
+            "state": type,
+            "tag": tag,
+          });
+        }
         break;
       case "serve":
         break;
